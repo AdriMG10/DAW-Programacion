2552b36c79558320a2d80172c2ad6827e7d412c1
Adri <115579621+AdriMG10@users.noreply.github.com>
2023-01-18 10:25:31 +0100

    1.0

--- actividad41/Actividad41.java
+++ actividad41/Actividad41.java
@@ -23,30 +23,54 @@ public class Actividad41 {
 
         System.out.println("Antes del método:");
         System.out.println(a[0]);
-        System.out.println(a[2]);
-        System.out.println(a[4]);
-        m(a);
+        System.out.println((a.length - 1)/2);
+        System.out.println(a.length - 1);
+        m(x, s, a);
         System.out.println("Después del método:");
         System.out.println(a[0]);
-        System.out.println(a[2]);
-        System.out.println(a[4]);
+        System.out.println((a.length - 1)/2);
+        System.out.println(a.length - 1);
     }
 
-    public static void m (int x) {
+    private static void m (int x) {
         x += 1;
     }
 
-    public static void m (int x, String s) {
+    private static void m (int x, String s) {
         s = s.toUpperCase();
+        m(x);                                       //Llamo al otro método
     }
 
-    public static void m (Double[] a) {
+    private static void m (int x, String s, Double[] a) {
         a[0] += 1;
-        a[2] += 1;
-        a[4] += 1;
+        a[(a.length - 1)/2] += 1;
+        a[a.length - 1] += 1;
+
+        m(x, s);                                    //Llamo al método anterior, que a su vez llama al anterior
     }
 
-    //final int M = 2;
-    //public static int[][] m2 (Double[] a, int M) {}
-    //No sé hacer más del 7
+    private static int[][] m2 (int x, String s, Double[] input) {
+        int n = input.length;
+        int m = 2;
+        int[][] output = new int[n][m];
+
+        m(x, s, input);
+
+        output[0][0] = (int)Math.floor(input[0]);                               //Convierto Double a int //También vale input[0].intValue();
+        output[0][1] = (int)Math.round(input[0]);
+
+        output[1][0] = (int)Math.floor(input[1]);
+        output[1][1] = (int)Math.round(input[1]);
+
+        output[2][0] = (int)Math.floor(input[2]);
+        output[2][1] = (int)Math.round(input[2]);
+
+        output[3][0] = (int)Math.floor(input[3]);
+        output[3][1] = (int)Math.round(input[3]);
+
+        output[4][0] = (int)Math.floor(input[4]);
+        output[4][1] = (int)Math.round(input[4]);
+
+        return output;
+    }
 }
