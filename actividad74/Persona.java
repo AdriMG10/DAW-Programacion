699953b580da0e16aaefa682d416287553e6509c
Adri <115579621+AdriMG10@users.noreply.github.com>
2023-05-30 11:25:29 +0200

    1.0

---
+++ actividad74/Persona.java
@@ -0,0 +1,37 @@
+package org.ieslosremedios.daw1.prog.ut7.actividades.actividad74;
+
+public class Persona {
+    String nombre;
+    String telefono;
+    String direccion;
+
+    public Persona(String nombre, String telefono, String direccion) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
